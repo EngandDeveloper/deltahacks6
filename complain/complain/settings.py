f0e37e396f4546cf4550b427fc694a278485cd4e
enganddeveloper <46913754+EngandDeveloper@users.noreply.github.com>
2020-01-25 12:43:26 -0500

    "Initial landing page is up"

--- complain/complain/settings.py
+++ complain/complain/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
+    #my apps
+    'mainpage',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'complain.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
